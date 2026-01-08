129aba9c2190dce262fced957c7c61828d7855c1
gregoridelrio <gregoridelrio@gmail.com>
2026-01-08 12:56:32 +0100

    add 14

--- query/queries.js
+++ query/queries.js
@@ -38,7 +38,7 @@ db.restaurants.find({"cuisine" : {"$ne" : "American"}, "grades.score" : {"$gt" :
 db.restaurants.find({"cuisine":{"$ne":"American"},"grades.grade":{"$eq":"A"}, "borough":{"$ne":"Brooklyn"}}, { "_id": 0 }).sort({"cuisine": -1})
 
 // 14. Trobar restaurant_id, name, borough i cuisine on el nom comença amb 'Wil'.
-
+db.restaurants.find({"name":/^Wil/}, { "restaurant_id":1, "name":1, "borough":1, "cuisine" :1, "_id": 0 })
 
 // 15. Trobar restaurant_id, name, borough i cuisine on el nom acaba en 'ces'.
 
