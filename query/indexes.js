f50159f4642e74f55711368fde8d5d4e162e6918
gregoridelrio <gregoridelrio@gmail.com>
2026-01-08 11:47:38 +0100

    add indexes

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,4 @@
 //Use this file to define indexes for your queries
+db.restaurants.createIndex({ borough: 1 });
+db.restaurants.createIndex({ cuisine: 1 });
+db.restaurants.createIndex({ name: 1 });
